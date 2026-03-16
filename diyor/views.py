c02f41860df9c50387111f1bc308b4397833eef9
Ne'matulloh Uktamov <123865261+unn-info-tech@users.noreply.github.com>
2026-03-16 15:33:39 +0500

    L

--- diyor/views.py
+++ diyor/views.py
@@ -8,12 +8,5 @@ def kitob_list(request):
     output = ', '.join([kitob.nom for kitob in kitoblar])
     return HttpResponse(output)
 
-def kitob_detail(request, pk):  
-    try:
-        kitob = Kitob.objects.get(pk=pk)
-        output = f"Nom: {kitob.nom}, Muallif: {kitob.mu allif}, Nashr Yili: {kitob.nashr_yili}, Narx: {kitob.narx}"
-        return HttpResponse(output) 
-    except Kitob.DoesNotExist:
-        return HttpResponse("Kitob topilmadi", status=404)
-    
+
     
