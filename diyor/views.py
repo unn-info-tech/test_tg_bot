6e512d1c4b3969291b9a6d8fc60bcb3ed3445c48
Nematulloh <unn.info.tech@gmail.com>
2026-03-16 15:32:56 +0500

    F

--- diyor/views.py
+++ diyor/views.py
@@ -8,3 +8,12 @@ def kitob_list(request):
     output = ', '.join([kitob.nom for kitob in kitoblar])
     return HttpResponse(output)
 
+def kitob_detail(request, pk):  
+    try:
+        kitob = Kitob.objects.get(pk=pk)
+        output = f"Nom: {kitob.nom}, Muallif: {kitob.mu allif}, Nashr Yili: {kitob.nashr_yili}, Narx: {kitob.narx}"
+        return HttpResponse(output) 
+    except Kitob.DoesNotExist:
+        return HttpResponse("Kitob topilmadi", status=404)
+    
+    
